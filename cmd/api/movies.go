d78ce7963a41b31c32e78a74864ff578940fa04d
divakaivan <timee98@outlook.com>
2025-04-20 15:18:43 +0900

    add helper func to read id from params

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -13,10 +13,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
